7a104df2eda8f55e842517d42f8b549ee33b8f08
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-27 22:33:29 -0200

    Adicionado sass para facilitar alterações específicas do framework foundation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
+var plugins = {
+  sass: require('gulp-sass'),
+  notify: require("gulp-notify")
+};
+
 var webFolder = 'web';
 
-gulp.task('sync', function () {
+gulp.task('sass', function() {
+  return gulp.src('app2.scss')
+      .pipe(plugins.sass(
+         {includePaths: ['./node_modules/foundation-sites/scss']}
+      ))
+      .pipe(gulp.dest('web/css'));
+});
+
+gulp.task('sync', ['sass'], function () {
   browserSync.init({
     server: {
       baseDir: webFolder
